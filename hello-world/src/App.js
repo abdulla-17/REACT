21e547e7fd39a302bb6870222f8b0029e0908a4a
abdulla-17 <abd26437@gmail.com>
2025-12-08 15:47:40 +0530

    Add initial project structure and implement basic functionality
    - Added logo image (logo512.png) for the application.
    - Created manifest.json for PWA support with app details and icons.
    - Added robots.txt to manage web crawler access.
    - Implemented App.css for styling with a greeting style.
    - Developed App.js component to display a greeting message.
    - Created App.test.js for initial testing of the App component.
    - Established index.css for global styles.
    - Set up index.js as the entry point for the application.
    - Included logo.svg for branding.
    - Implemented reportWebVitals.js for performance tracking.
    - Configured setupTests.js for testing environment setup.

---
+++ hello-world/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+function App() {
+  let greeting = "Hello World";
+  console.log(greeting);
+  return (
+  <div>
+  <h1 className="greetingStyle">{greeting}</h1>
+  </div>
+  );
+}
+export default App;
